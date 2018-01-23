91d946627a1140889fe1b418da09785d21a33211
Mónica del Corral <monica.delcorral@wildoom.com>
2018-01-23 16:14:55 +0100

    WIP Homepage

--- style.js
+++ style.js
@@ -64,7 +64,10 @@ oxfordFlippedApp.config = {}
 oxfordFlippedApp.homepage = function(data) {
 	console.log("Homepage");
 	$('body').addClass('htmlReady');
-	var bookTitle = data.title;
-	var html = '<div id="oxfl-general-home" class="oxfl-general"> <h1 class="oxfl-title1">'+bookTitle+'</h1> <div id="oxfl-coins"></div> <button class="oxfl-notifications"> <div class="oxfl-notifications-badge">5</div> </button> <div class="oxfl-home-menu"> <div class="oxfl-home-menu-item"> <button class="oxfl-monster oxfl-monster-1" id="oxfl-goto-gradebook"> <span>Gradebook</span> </button> </div> <div class="oxfl-home-menu-item"> <button class="oxfl-monster oxfl-monster-2" id="oxfl-goto-prepare"> <span>Prepare</span> </button> </div> <div class="oxfl-home-menu-item"> <button class="oxfl-monster oxfl-monster-3" id="oxfl-goto-marketplace"> <span>Marketplace</span> </button> <div class="oxfl-bubble-hello"> <span class="oxfl-bubble-hello-text">Hola, </span> <span class="oxfl-bubble-hello-name"></span> </div> </div> </div> </div>';
+	var bookTitle = data.title,
+			username = 'Federico',
+			totalCoins = '1.000.000';
+	// TODO Decide if put coins outside homepage, to make coins transversal.
+	var html = '<div id="oxfl-coins"><div id="oxfl-coins-icon"></div><div id="oxfl-coins-total">'+totalCoins+'</div></div> <div id="oxfl-general-home" class="oxfl-general"> <h1 class="oxfl-title1">'+bookTitle+'</h1> <button class="oxfl-notifications"> <div class="oxfl-notifications-badge">5</div> </button> <div class="oxfl-home-menu"> <div class="oxfl-home-menu-item"> <button class="oxfl-monster oxfl-monster-1" id="oxfl-goto-gradebook"> <span>Gradebook</span> </button> </div> <div class="oxfl-home-menu-item"> <button class="oxfl-monster oxfl-monster-2" id="oxfl-goto-prepare"> <span>Prepare</span> </button> </div> <div class="oxfl-home-menu-item"> <button class="oxfl-monster oxfl-monster-3" id="oxfl-goto-marketplace"> <span>Marketplace</span> </button> <div class="oxfl-bubble-hello"> <span class="oxfl-bubble-hello-text">Hola, </span> <span class="oxfl-bubble-hello-name">'+username+'</span> </div> </div> </div> </div>';
 	$('body').prepend(html);
 }
