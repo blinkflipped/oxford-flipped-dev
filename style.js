79be1174547a8f04668d0d135b99f99e7fe17204
Mónica del Corral <monica.delcorral@wildoom.com>
2018-02-01 20:15:26 +0100

    User permissions in Chapters

--- style.js
+++ style.js
@@ -172,7 +172,7 @@ oxfordFlippedApp.loadChapters = function(data,currentEpisode) {
 				chapterStateID = '2',
 				chapterUrlHTML = (oxfordFlippedApp.config.isStudent && (chapterLockStatus === 8 || chapterLockStatus === 2)) ? '' : 'class="oxfl-js-load-chapter" data-url="'+chapter.url+'"',
 				chapterListItem = document.createElement('div'),
-				chapterinnerHTML =  '<article class="oxfl-chapter" data-id="'+chapterID+'"> <div class="oxfl-chapter-header"> <div class="oxfl-chapter-header-top"> <h2 class="oxfl-title3"> <a href="javascript:void(0)" '+chapterUrlHTML+'> Chapter '+chapterNumber+' </a> </h2> <div class="oxfl-chapter-header-top-right">'+chapterActions+'</div> </div> <h3 class="oxfl-title4"><a href="javascript:void(0)" '+chapterUrlHTML+'>'+chapterTitle+'</a></h3> </div> <a href="javascript:void(0)" '+chapterUrlHTML+'> <div class="oxfl-chapter-image-wrapper"> <div class="oxfl-label oxfl-label-'+chapterStateID+'">'+chapterState+'</div> '+chapterImageCode+' </div> </a> </article>';
+				chapterinnerHTML =  '<article class="oxfl-chapter '+chapterLockClass+'" data-id="'+chapterID+'"> <div class="oxfl-chapter-header"> <div class="oxfl-chapter-header-top"> <h2 class="oxfl-title3"> <a href="javascript:void(0)" '+chapterUrlHTML+'> Chapter '+chapterNumber+' </a> </h2> <div class="oxfl-chapter-header-top-right">'+chapterActions+'</div> </div> <h3 class="oxfl-title4"><a href="javascript:void(0)" '+chapterUrlHTML+'>'+chapterTitle+'</a></h3> </div> <a href="javascript:void(0)" '+chapterUrlHTML+'> <div class="oxfl-chapter-image-wrapper"> <div class="oxfl-label oxfl-label-'+chapterStateID+'">'+chapterState+'</div> '+chapterImageCode+' </div> </a> </article>';
 
 		chapterListItem.className = 'oxfl-chapter-item';
 		chapterListItem.innerHTML = chapterinnerHTML;
@@ -234,8 +234,9 @@ $(document).ready(function() {
 	$('body').on('click', '.oxfl-js-load-chapter', function(e) {
 
 		e.preventDefault();
-		console.log("AAAAA");
+		var chapterUrl = $(this).data('url');
 
+		showIframe(chapterUrl, 1000, 700, false, true, false, '');
 
 	});
 
