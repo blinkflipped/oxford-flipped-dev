7f110bfe7c8bd95089f8ac5ad503926e694d8aa6
Mónica del Corral <monica.delcorral@wildoom.com>
2018-02-01 19:48:43 +0100

    User permissions in Chapters

--- style.js
+++ style.js
@@ -76,7 +76,7 @@ var oxfordFlippedApp = window.oxfordFlippedApp || {};
 oxfordFlippedApp.config = {}
 
 oxfordFlippedApp.config.carouselOpt = {arrows: true, dots: true, infinite: false}
-oxfordFlippedApp.config.isStudent = false;
+oxfordFlippedApp.config.isStudent = true;
 
 oxfordFlippedApp.fontSizeResize = function(elements) {
 	if (elements.length < 0) {
