952a4f13ea1cb43fa798b5371ce03247bef4e9c2
Mónica del Corral <monica.delcorral@wildoom.com>
2018-04-23 17:47:35 +0200

    - Test Info button

--- style.js
+++ style.js
@@ -887,7 +887,8 @@ oxfordFlippedApp.homepage = function(data) {
 			console.log(subunit.onclickTitle);
 			var fnstring = subunit.onclickTitle;
 			var fn = window[fnstring];
-
+			console.log("BB");
+			console.log(fn);
 			$('.oxfl-js-open-info').show().on('click', function() {
 				console.log("AA");
 				console.log(subunit.onclickTitle);
