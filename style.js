a822064d42457e3e53703d64e0b565030d0d76ce
Mónica del Corral <monica.delcorral@wildoom.com>
2018-04-25 14:25:14 +0200

    GravatarMarketplace States and modal WIP - Info modal WIP

--- style.js
+++ style.js
@@ -1178,7 +1178,7 @@ oxfordFlippedApp.loadMarketplaceList = function(data,type,itemperpage) {
 											resourceurl = resource.url,
 											resourceStateNew = (typeof window.actividades[resourceId] === 'undefined'),
 											resourceStateClass = (resourceStateNew) ? 'oxfl-resource-locked' : '',
-											resourceOnClick = (!oxfordFlippedApp.config.isStudent || (oxfordFlippedApp.config.isStudent && !resourceStateNew)) ? resource.onclickTitle : 'oxfordFlippedApp.oxflMarketplaceModal('+resourceValue+', '+resourceTitle+','+resourceDescription+','+resourceId+')',
+											resourceOnClick = (!oxfordFlippedApp.config.isStudent || (oxfordFlippedApp.config.isStudent && !resourceStateNew)) ? resource.onclickTitle : 'oxfordFlippedApp.oxflMarketplaceModal('+resourceValue+', "'+resourceTitle+'","'+resourceDescription+'","'+resourceId+'")',
 											resourceListItem = document.createElement('div');
 											resourceListItem.className = 'oxfl-resource-item';
 											console.log("Marketplaceresources");
