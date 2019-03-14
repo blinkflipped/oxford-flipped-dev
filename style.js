a5fb9e759c85373f5a4d66527aa44d4ca9a199e5
Mónica del Corral <monica.delcorral@wildoom.com>
2019-03-14 17:42:26 +0100

    WIP Teacher resources

--- style.js
+++ style.js
@@ -2478,17 +2478,17 @@ oxfordFlippedApp.loadClassResources = function(updateHash) {
 	var resourceList = document.createDocumentFragment();
 	var totalResources = 0;
 	$.each(oxfordFlippedApp.bookData.units, function(i, unit){
-		if (i != oxfordFlippedApp.config.ConfigActivityIndex) {
+		if (i !== oxfordFlippedApp.config.ConfigActivityIndex) {
 			var subunits = unit.subunits,
 					title = unit.title,
 					description = unit.description,
-					image = (resource.image !== '') ? '<img src="'+resource.image+'" alt="'+title+'">' : '',
+					image = (unit.image !== '') ? '<img src="'+resource.image+'" alt="'+title+'">' : '',
 					id = unit.id;
-
 					totalResources++;
+
 					var resourceListItem = document.createElement('div');
 							resourceListItem.className = 'oxfl-resource-item oxfl-resource-item-2';
-					resourceListItem.innerHTML = '<article class="oxfl-resource"> <a href="javascript:void(0)" data-resource-unit-id="'+id+'"><header class="oxfl-resource-header"> <h2 class="oxfl-title2">'+title1+'</h2><h3 class="oxfl-title4">'+description+'</h3></header> <div class="oxfl-resource-image-wrapper"><div class="oxfl-resource-image-cover"></div> <div class="oxfl-resource-image-wrapper-img">'+image+'</div> </div> </a> </article>';
+					resourceListItem.innerHTML = '<article class="oxfl-resource"> <a href="javascript:void(0)" data-resource-unit-id="'+id+'"><header class="oxfl-resource-header"> <h2 class="oxfl-title2">'+title+'</h2><h3 class="oxfl-title4">'+description+'</h3></header> <div class="oxfl-resource-image-wrapper"><div class="oxfl-resource-image-cover"></div> <div class="oxfl-resource-image-wrapper-img">'+image+'</div> </div> </a> </article>';
 					resourceList.appendChild(resourceListItem);
 		}
 	});
